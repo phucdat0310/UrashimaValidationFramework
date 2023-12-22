9b59230259fbe883c6073476da4c09ff499d5176
phucdat <154251020+phucdat0310@users.noreply.github.com>
2023-12-22 22:40:36 +0700

    Add common extension functions

---
+++ UrashimaValidation/Functions/DateFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrashimaValidation.Functions
+{
+    public static class DateFunctions
+    {
+        /// <summary>
+        /// Checks, if the value is today
+        /// </summary>
+        /// <param name="value">The date to check</param>
+        /// <returns>True if the date of the value is today</returns>
+        public static bool IsToday(this DateTime value)
+        {
+            DateTime today = DateTime.Today;
+            return today.Year == value.Year &&
+                today.Month == value.Month &&
+                today.Day == value.Day;
+        }
+    }
+}
---
+++ UrashimaValidation/Functions/ListFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrashimaValidation.Functions
+{
+    public static class ListFunctions
+    {
+        /// <summary>
+        /// Is the value in a list
+        /// </summary>
+        /// <typeparam name="T">The type</typeparam>
+        /// <param name="value">The value to check</param>
+        /// <param name="values">The list to check</param>
+        /// <returns>True if the list contains the value. Else false.</returns>
+        public static bool In<T>(
+            this T value,
+            IEnumerable<T> values)
+        {
+            return values.Contains(value: value);
+        }
+    }
+}
